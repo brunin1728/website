8504a1dfc1f3df61a1f4bc3b252b7daed0b64af7
blueinteligencia <bruno.blueinteligencia@gmail.com>
2018-09-20 17:46:08 -0300

    BNCTECNOLOGIA

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { InAppBrowser, InAppBrowserOptions } from '@ionic-native/in-app-browser';
+import { OneSignal } from '@ionic-native/onesignal';
+import { SplashScreen } from '@ionic-native/splash-screen';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +10,50 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(
+    public navCtrl: NavController,
+    private iab: InAppBrowser,
+    private oneSignal: OneSignal,
+    splashScreen: SplashScreen
+  ) {
 
+  this.push();
+  this.abrir();
+    splashScreen.hide();
   }
+
+
+abrir(){
+  const options: InAppBrowserOptions = {
+    zoom: 'no',
+    location: 'no'
+  }
+
+ const browser = this.iab.create('https://www.bnctecnologia.com.br', '_blank', options);
+
+}
+
+
+push(){
+  this.oneSignal.startInit('dd743356-b81b-4ea8-86da-993d273c820d', '1001915559416');
+
+this.oneSignal.inFocusDisplaying(this.oneSignal.OSInFocusDisplayOption.InAppAlert);
+
+this.oneSignal.handleNotificationReceived().subscribe((msg) => {
+  alert(msg);
+});
+
+this.oneSignal.handleNotificationOpened().subscribe(() => {
+  // do something when a notification is opened
+});
+
+this.oneSignal.endInit();
+
+
+}
+
+
+
+
 
 }
